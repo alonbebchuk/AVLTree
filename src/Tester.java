3dafadb18f50463be78fbcae8239178f63da21b3
Alon Bebchuk <alon.bebchuk@gmail.com>
2021-05-03 17:25:09 +0300

    lol

--- src/Tester.java
+++ src/Tester.java
@@ -2,50 +2,30 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.function.IntPredicate;
 
-
 public class Tester {
 
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
-
-        // tree empty on init
-        if (!tree.empty()) {
-            System.out.println("tree not empty on init");
-        }
-
         tree.insert(3, true);
-
-        // tree not empty after add
-        if (tree.empty()) {
-            System.out.println("tree empty after insert");
-        }
-
+        Printer.print(tree.getRoot());
         tree.insert(10, true);
+        Printer.print(tree.getRoot());
         tree.insert(8, true);
-
-        // -2 +1 rotation
-        if (tree.getRoot().getKey() != 8) {
-            System.out.println("insert - wrong -2 +1 rotation");
-        }
-
+        Printer.print(tree.getRoot());
         tree.insert(11, true);
+        Printer.print(tree.getRoot());
         tree.insert(12, true);
-
-        // -2 -1 rotation
-        if (tree.getRoot().getRight().getKey() != 11) {
-            System.out.println("insert - wrong -2 -1 rotation");
-        }
-
+        Printer.print(tree.getRoot());
         tree.insert(2, true);
+        Printer.print(tree.getRoot());
         tree.insert(5, true);
+        Printer.print(tree.getRoot());
         tree.insert(4, true);
+        Printer.print(tree.getRoot());
         tree.insert(6, true);
+        Printer.print(tree.getRoot());
         tree.insert(7, true);
-
-        // +2 -1 rotation
-        if (tree.getRoot().getKey() != 5) {
-            System.out.println("insert - wrong +2 -1 rotation");
-        }
+        Printer.print(tree.getRoot());
     }
 }
 
