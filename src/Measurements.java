7e3a82f74c98b3683624f7690ca9a360316b2291
Alon Bebchuk <alon.bebchuk@gmail.com>
2021-05-24 00:50:51 +0300

    finished

--- src/Measurements.java
+++ src/Measurements.java
@@ -1,55 +1,56 @@
 import java.util.*;
 import java.lang.Math;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 
 public class Measurements {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            double[][] results = compareXors2();
-            print2D(results);
-            System.out.println();
-//        compareTrees();
+        System.out.println("Compare Xors Test");
+        compareXorsTest();
+        System.out.println("Compare Trees Test");
+        compareTreesTest();
+    }
+
+    private static List<Integer> randomUniqueList(int desiredSize) {
+        List<Integer> randomUniqueList = new ArrayList<>();
+        Set<Integer> inventory = new HashSet<>();
+        Random rand = new Random();
+
+        int counter = 0, randValue;
+
+        while (desiredSize > counter++) {
+            randValue = rand.nextInt();
+
+            if (inventory.add(randValue)) {
+                randomUniqueList.add(randValue);
+            }
         }
+
+        return randomUniqueList;
     }
 
-    public static void print2D(double mat[][])
-    {
-        // Loop through all rows
-        for (double[] row : mat)
+    private static double[][] compareXors() {
+        double result[][] = new double[5][4];
 
-            // converting each row as string
-            // and then printing in a separate line
-            System.out.println(Arrays.toString(row));
-    }
-
-    private static double[][] compareXors2() {
-        double results[][] = new double[5][];
-
-        List<Integer> inputs = IntStream.rangeClosed(1, 100000).boxed().collect(Collectors.toList());
-        Collections.shuffle(inputs);
+        List<Integer> inputs = randomUniqueList(2500);
         Random rand = new Random();
 
         AVLTree avlTree = new AVLTree();
 
         long start, end;
         double totalPrefixXor, totalFirst100PrefixXor, totalSuccPrefixXor, totalFirst100SuccPrefixXor;
+        int[] keys;
 
         for (int i = 1; i <= 5; i++) {
-            for (int input : inputs.subList(500 * (i-1), 500 * i)) {
+            totalPrefixXor = totalFirst100PrefixXor = totalSuccPrefixXor = totalFirst100SuccPrefixXor = 0;
+
+            for (int input : inputs.subList(500 * (i - 1), 500 * i)) {
                 avlTree.insert(input, rand.nextBoolean());
             }
 
-            totalPrefixXor = totalFirst100PrefixXor = totalSuccPrefixXor = totalFirst100SuccPrefixXor = 0;
-
-            int[] keys = avlTree.keysToArray();
+            keys = avlTree.keysToArray();
 
             for (int j = 0; j < 500 * i; j++) {
-                System.nanoTime();
-                System.nanoTime();
-
                 start = System.nanoTime();
                 avlTree.prefixXor(keys[j]);
                 end = System.nanoTime();
@@ -58,9 +59,6 @@ public class Measurements {
                 if (j < 100) {
                     totalFirst100PrefixXor += end - start;
                 }
-
-                System.nanoTime();
-                System.nanoTime();
 
                 start = System.nanoTime();
                 avlTree.succPrefixXor(keys[j]);
@@ -72,89 +70,169 @@ public class Measurements {
                 }
             }
 
-            results[i-1] = new double[4];
-            results[i-1][0] = totalPrefixXor / (500 * i);
-            results[i-1][1] = totalFirst100PrefixXor / 100;
-            results[i-1][2] = totalSuccPrefixXor / (500 * i);
-            results[i-1][3] = totalFirst100SuccPrefixXor / 100;
+            result[i - 1][0] = totalPrefixXor / (500 * i);
+            result[i - 1][1] = totalFirst100PrefixXor / 100;
+            result[i - 1][2] = totalSuccPrefixXor / (500 * i);
+            result[i - 1][3] = totalFirst100SuccPrefixXor / 100;
         }
 
-        return results;
+        return result;
     }
 
-    public static int[] createArr(int size) {
-        int[] arr = new int[5000];
-        int i = 13;
-        int counter = 0;
-        while (true) {
+    public static void print2D(double mat[][]) {
+        for (double[] row : mat) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void compareXorsTest() {
+        double[][] averageResult = new double[5][4], result;
+        int attempts = 1000;
+
+        // warm-up
+        for (int i = 0; i < 50; i++) {
+            compareXors();
+        }
+
+        for (int i = 0; i < attempts; i++) {
+            result = compareXors();
+
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 4; k++) {
+                    averageResult[j][k] += result[j][k];
+                }
+            }
+        }
+
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 4; k++) {
+                averageResult[j][k] /= attempts;
+            }
+        }
+
+        print2D(averageResult);
+    }
+
+    public static List<Integer> balancedList(int desiredSize) {
+        List<Integer> balancedList = new ArrayList<>();
+
+        for (int i = 13; i >= 0; i--) {
             for (int j = 0; j < (int) Math.pow(2, 13 - i + 1); j++) {
-                if (counter == size - 1) {
-                    break;
-                }
                 if (j % 2 == 0) {
-                    arr[counter] = (int) Math.ceil(Math.pow(2, i - 1) * (j + 1));
-                    counter++;
+                    balancedList.add((int) Math.ceil(Math.pow(2, i - 1) * (j + 1)));
+                    if (balancedList.size() == desiredSize) {
+                        return balancedList;
+                    }
                 }
-            }
-            i--;
-            if (i == 0) {
-                break;
             }
         }
-        return Arrays.copyOf(arr, size);
+
+        return null;
     }
 
-    public static void compareTrees() {
+    public static double[][] compareTrees() {
+        double result[][] = new double[5][6];
+
+        AVLTree avlTree;
+        NonAVLTree nonAVLTree;
+
         Random rand = new Random();
-        NonAVLTree normalTree = new NonAVLTree();
-        AVLTree avlTree = new AVLTree();
-        long first, second;
-        double avlArithmatic = 0, normalArithmatic = 0, avlBalanced = 0, normalBalanced = 0, avlRandom = 0, normalRandom = 0;
-        for (int i = 0; i < 5; i++) {
-            System.out.println("For " + 1000 * (i + 1) + " elements");
-            for (int j = 0; j < 1000 * (i + 1); j++) {
-                first = System.currentTimeMillis();
-                normalTree.insert(j, false);
-                second = System.currentTimeMillis();
-                normalArithmatic += second - first;
-                first = System.currentTimeMillis();
-                avlTree.insert(j, false);
-                second = System.currentTimeMillis();
-                avlArithmatic += second - first;
+
+        long start;
+        double avlArithmatic, normalArithmatic, avlRandom, normalRandom, avlBalanced, normalBalanced;
+        List<Integer> inputs;
+
+        for (int i = 1; i <= 5; i++) {
+            // arithmetic
+            avlTree = new AVLTree();
+            nonAVLTree = new NonAVLTree();
+
+            start = System.nanoTime();
+            for (int j = 0; j < 1000 * i; j++) {
+                avlTree.insert(j, rand.nextBoolean());
             }
-            avlArithmatic /= (i + 1) * 1000;
-            normalArithmatic /= (i + 1) * 1000;
-            System.out.println("Average time per insert for normal tree arithmetic sequence: " + normalArithmatic + " milliseconds");
-            System.out.println("Average time per insert for avl tree arithmetic sequence: " + avlArithmatic + " milliseconds");
-            for (int j = 0; j < 1000 * (i + 1); j++) {
-                first = System.currentTimeMillis();
-                normalTree.insert(rand.nextInt(), false);
-                second = System.currentTimeMillis();
-                normalRandom += second - first;
-                first = System.currentTimeMillis();
-                avlTree.insert(rand.nextInt(), false);
-                second = System.currentTimeMillis();
-                avlRandom += second - first;
+            avlArithmatic = System.nanoTime() - start;
+
+            start = System.nanoTime();
+            for (int j = 0; j < 1000 * i; j++) {
+                nonAVLTree.insert(j, rand.nextBoolean());
             }
-            avlRandom /= (i + 1) * 1000;
-            normalRandom /= (i + 1) * 1000;
-            System.out.println("Average time per insert for normal tree random sequence: " + normalRandom + " milliseconds");
-            System.out.println("Average time per insert for avl tree random sequence: " + avlRandom + " milliseconds");
-            int[] balancedArr = createArr((i + 1) * 1000);
-            for (int j = 0; j < 1000 * (i + 1); j++) {
-                first = System.currentTimeMillis();
-                normalTree.insert(balancedArr[j], false);
-                second = System.currentTimeMillis();
-                normalRandom += second - first;
-                first = System.currentTimeMillis();
-                avlTree.insert(balancedArr[j], false);
-                second = System.currentTimeMillis();
-                avlRandom += second - first;
+            normalArithmatic = System.nanoTime() - start;
+
+            result[i - 1][0] = avlArithmatic / (1000 * i);
+            result[i - 1][1] = normalArithmatic / (1000 * i);
+
+            // balanced
+            inputs = balancedList(1000 * i);
+
+            avlTree = new AVLTree();
+            nonAVLTree = new NonAVLTree();
+
+            start = System.nanoTime();
+            for (int input : inputs) {
+                avlTree.insert(input, rand.nextBoolean());
             }
-            avlBalanced /= (i + 1) * 1000;
-            normalBalanced /= (i + 1) * 1000;
-            System.out.println("Average time per insert for normal tree balanced sequence: " + normalBalanced + " milliseconds");
-            System.out.println("Average time per insert for avl tree balanced sequence: " + avlBalanced + " milliseconds");
+            avlBalanced = System.nanoTime() - start;
+
+            start = System.nanoTime();
+            for (int input : inputs) {
+                nonAVLTree.insert(input, rand.nextBoolean());
+            }
+            normalBalanced = System.nanoTime() - start;
+
+            result[i - 1][2] = avlBalanced / (1000 * i);
+            result[i - 1][3] = normalBalanced / (1000 * i);
+
+            // random
+            avlTree = new AVLTree();
+            nonAVLTree = new NonAVLTree();
+
+            inputs = randomUniqueList(1000 * i);
+
+            start = System.nanoTime();
+            for (int input : inputs) {
+                avlTree.insert(input, rand.nextBoolean());
+            }
+            avlRandom = System.nanoTime() - start;
+
+            start = System.nanoTime();
+            for (int input : inputs) {
+                nonAVLTree.insert(input, rand.nextBoolean());
+            }
+            normalRandom = System.nanoTime() - start;
+
+            result[i - 1][4] = avlRandom / (1000 * i);
+            result[i - 1][5] = normalRandom / (1000 * i);
         }
+
+        return result;
+    }
+
+    public static void compareTreesTest() {
+        double[][] averageResult = new double[5][6], result;
+        int attempts = 1000;
+
+        // warm-up
+        for (int i = 0; i < 50; i++) {
+            compareTrees();
+        }
+
+        for (int i = 0; i < attempts; i++) {
+            result = compareTrees();
+
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 6; k++) {
+                    averageResult[j][k] += result[j][k];
+                }
+            }
+        }
+
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 6; k++) {
+                averageResult[j][k] /= attempts;
+            }
+        }
+
+        print2D(averageResult);
     }
 }
